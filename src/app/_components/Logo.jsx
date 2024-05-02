5885885c5fd29b8a29bae7100399de307513de52
Dominik Frackowiak <86988772+DominikFrackowiak@users.noreply.github.com>
2024-05-02 21:55:21 +0200

    symbol // changed into &#8725;

--- src/app/_components/Logo.jsx
+++ src/app/_components/Logo.jsx
@@ -14,7 +14,9 @@ export default function Logo({ locale }) {
 	return (
 		<Link href={`/${locale}/?theme=${theme}`}>
 			<TextToAnimate delay={0.2}>
-				<span className={`${styles.logo} bold dark`}>//dominik_frackowiak</span>
+				<span className={`${styles.logo} bold dark`}>
+					&#8725;&#8725;dominik_frackowiak
+				</span>
 			</TextToAnimate>
 		</Link>
 	)

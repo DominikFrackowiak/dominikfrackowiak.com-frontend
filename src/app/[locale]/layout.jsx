fc38c59540ad9b4631f314a256ff6bb6d40d69fc
DominikFrackowiak <rorschach@op.pl>
2024-05-21 22:18:41 +0200

    revalidate constant added

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -7,7 +7,7 @@ import Providers from '../providers'
 
 const robotoMono = Roboto_Mono({ subsets: ['latin'] })
 
-// export const revalidate = 3600
+export const revalidate = 1000
 
 export default function RootLayout({ children, params: { locale } }) {
 	return (

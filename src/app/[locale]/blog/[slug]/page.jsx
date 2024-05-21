4fba6e0cb01feaa79f8f54115be92247a3f7b4ee
DominikFrackowiak <rorschach@op.pl>
2024-05-21 09:06:44 +0200

    missong key prop added

--- src/app/[locale]/blog/[slug]/page.jsx
+++ src/app/[locale]/blog/[slug]/page.jsx
@@ -7,47 +7,42 @@ export default async function BlogPage({ params: { slug } }) {
 	const blog = data.posts.nodes[0]
 	// console.log(blog)
 
-function extractCodeFromHTML(htmlString) {
-	const regex = /<pre class="wp-block-code"><code>(.*?)<\/code><\/pre>/gs
-	const match = regex.exec(htmlString)
-	return match ? match[1] : ''
-}
-
-
-function handleHeadings(element){
-	switch(element.level){
-		case 1: return <h1 key={uuid()}>{element.content}</h1>
-		return
-		case 2: return <h2 key={uuid()}>{element.content}</h2>
+	function extractCodeFromHTML(htmlString) {
+		const regex = /<pre class="wp-block-code"><code>(.*?)<\/code><\/pre>/gs
+		const match = regex.exec(htmlString)
+		return match ? match[1] : ''
 	}
-}
 
-
+	function handleHeadings(element) {
+		switch (element.level) {
+			case 1:
+				return <h1 key={uuid()}>{element.content}</h1>
+				return
+			case 2:
+				return <h2 key={uuid()}>{element.content}</h2>
+		}
+	}
 
 	// console.log(blog.blocks)
 	return (
 		<main className='main'>
 			<h1>{blog.title}</h1>
 			{blog.blocks.map(block => {
-				
 				if (block.name === 'core/paragraph') {
 					return <p key={uuid()}>{block.attributes.content}</p>
 				}
 				if (block.name === 'core/code') {
-					// block.htmlContent
-					// const extractedCode = extractCodeFromHTML(block.htmlContent)
-					// console.log( extractedCode)
-					// return (
-					// 	<code key={uuid()}>
-					// 		{extractedCode}
-					// 	</code>
-					// )
-					<div dangerouslySetInnerHTML={{__html: block.htmlContent}}/> 
+					return (
+						<div
+							key={uuid()}
+							dangerouslySetInnerHTML={{ __html: block.htmlContent }}
+						/>
+					)
 				}
-				if(block.name==='core/heading'){
-					 const heading = handleHeadings(block.attributes)
-						
-						return heading
+				if (block.name === 'core/heading') {
+					const heading = handleHeadings(block.attributes)
+
+					return heading
 				}
 			})}
 		</main>

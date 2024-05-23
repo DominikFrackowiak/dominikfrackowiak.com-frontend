3b6c4a2febd5e0d4f831ee321cb3f25ddf86170b
DominikFrackowiak <rorschach@op.pl>
2024-05-23 13:22:45 +0200

    fetch only those tags that contains a specofic tag functionality added

--- src/app/[locale]/blog/page.jsx
+++ src/app/[locale]/blog/page.jsx
@@ -1,34 +1,45 @@
 import Link from 'next/link'
 
 import getAllPostsData from '@/lib/getAllPostsData'
+import getPostsByTag from '@/lib/getPostsByTag'
+import styles from './blog.module.scss'
 
-export default async function AllBlogsPage({ params }) {
-	const { locale } = params
+export default async function AllBlogsPage({
+	params: { locale },
+	searchParams: { tag },
+}) {
+	let data
+	console.log(tag)
+	if (tag === undefined) {
+		data = await getAllPostsData(locale)
+	} else {
+		data = await getPostsByTag(locale, tag)
+	}
 
-	const data = await getAllPostsData(locale)
 	const allBlogs = data.posts.nodes
-	
-	console.log(allBlogs)
+
+	// console.log(allBlogs)
 
 	return (
 		<main className='main' style={{ zIndex: 1 }}>
-			<ul>
+			<ul className={styles.list}>
 				{allBlogs.map(blog => (
 					<li key={blog.id} style={{ marginBottom: '30px' }}>
 						<Link href={`blog/${blog.slug}`}>
-							<h2>{blog.title}</h2>
-							<p>
+							<h2 className={`${styles.heading2} underline`}>{blog.title}</h2>
+							<p className={styles.paragraph}>
 								<small>
 									{blog.blocks[0].attributes.content.slice(0, 100)}...
 								</small>
 							</p>
-							<small>Read more</small>
+							<small className={styles.readMore}>Read more</small>
 						</Link>
-						<div style={{display: 'flex', gap: '20px'}}>
-							
+						<div style={{ display: 'flex', gap: '20px' }}>
 							{blog.tags.nodes.map(tag => (
-								<button key={tag.name}>
-									<small>{tag.name}</small>
+								<button key={tag.name} className={styles.button}>
+									<Link href={`/${locale}/blog?tag=${tag.name}`}>
+										<small>{tag.name}</small>
+									</Link>
 								</button>
 							))}
 						</div>
